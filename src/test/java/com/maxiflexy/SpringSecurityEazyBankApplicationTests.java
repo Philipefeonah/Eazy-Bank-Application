55910d112959b186dc4843bad49dc9fd19757dab
Philipefeonah <163314450+Philipefeonah@users.noreply.github.com>
2024-08-12 14:48:53 +0100

    first commit

---
+++ src/test/java/com/maxiflexy/SpringSecurityEazyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiflexy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityEazyBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
